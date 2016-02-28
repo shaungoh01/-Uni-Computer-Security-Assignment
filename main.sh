696a272e8ad6db3f639b5dcba4c4b21c086cb787
shaun <shaungoh01@gmail.com>
2016-02-28 18:24:39 +0800

    added rail decrytion and save result for rail. decrytion cant handle mutil row yet so use file1.txt or a.txt

--- main.sh
+++ main.sh
@@ -8,6 +8,7 @@ echo --------------------------------------------------
 echo Enter 1 for Key cipher encryption
 echo Enter 2 for Rails
 echo Enter 3 for Key cipher decryption
+echo Enter 4 for Rails decryption
 echo Enter 99 for exit
 read uc
 clear
@@ -55,6 +56,10 @@ if [ $uc -eq 1 -o $uc -eq 3 ]; then
 elif [ $uc -eq 2 ]; then
 echo How many row you wan?
 read row
+echo Do you wan to save this?$'('Enter 1 for yes and any number for no$')'
+read ucs
+
+resultS=""
 
 while IFS='' read -r line || [[ -n $line ]]; do
 ##OUT=`echo "THIS IS A SIMPLE LAB" | tr $line $C`
@@ -94,45 +99,100 @@ do
 	echo $result
 done
 
+resultS=$resultS$result$'\r'
 
 done < "$1"
+
+if [ $ucs -eq 1 ]; then
+        echo $resultS > "encrypted_data.txt"
+fi
 
 elif [ $uc -eq 4 ]; then
 echo How many row you need to decrypt?
 read row
+echo Do you wan to save this?$'('Enter 1 for yes and any number for no$')'
+read ucs
 
 while IFS='' read -r line || [[ -n $line ]]; do
 ##OUT=`echo "THIS IS A SIMPLE LAB" | tr $line $C`
 ##echo $OUT
 
+#echo line - $line
 INPUT="${line// /}"
+#echo input-$INPUT
 SIZE=${#INPUT} #find size of the input
 row1=$[$row-1] #just row -1 due to starting from 0
 result=""
-
 for r in `seq 0 $row1`;
 do
         P=$[$row1*2] #p stand for primary gap
         S=$[$r*2] #s stand for second
         F=$[$P-S] #f stand for first
         if [ "$F" == "$P" ] || [ "$S" == "$P" ] ; then #if is the top and bttom row, no second gap
-                NROW[$r]=$[($SIZE-1-$r)/$P]
+                x1=0 #x1 for finding possition of the char that need to be sub
+                for x in `seq 0 $[($SIZE-1-$r)/$P]`; #take the start of the row and devide and see how many char in the row
+                do
+                        SUBSTRING=${INPUT:x:1}
+                        NROW[$r]="${NROW[$r]}$SUBSTRING" #glue result together
+			#echo nrow - ${NROW[$r]}
+                done
+		INPUT=${INPUT:x+1:${#INPUT}}
+		#echo input - $INPUT
+		#echo final nrow - ${NROW[$r]}
         else
                 x1=$r
+		xx=0
 		t=0 #to trigger between first and second gap
-                while (("$x1" < "$SIZE" )); do
+                while (("$x1" < "$SIZE-1" )); do
+			SUBSTRING=${INPUT:xx:1}
+			#echo sub - $SUBSTRING
+                        NROW[$r]="${NROW[$r]}$SUBSTRING"
+			#echo nrow - ${NROW[$r]}
                 if [ $((t%2)) -eq 0 ]; then
                         x1=$[$x1+$F]
                 else
                         x1=$[$x1+$S]
                 fi
+		xx=$[$xx+1]
                 t=$[$t+1]
+		#echo x1 = $x1
                 done
-		NROW[$r]=$t
+		INPUT=${INPUT:xx:${#INPUT}}
+		#echo input - $INPUT
+		#echo nrow - ${NROW[$r]}
         fi
 done
 
+needsub=${NROW[0]}
+Result=${needsub:0:1}
+NROW[0]=${needsub:1:${#needsub}}
+T=0
+for((i=1 ; i<$SIZE+1 ; i+=3));
+do
+	if [ $((T%2)) -eq 0 ]; then
+        	for r in `seq 1 $row1`;
+		do
+		needsub=${NROW[$r]}
+		Result=$Result${needsub:0:1}
+		NROW[$r]=${needsub:1:${#needsub}}
+		#echo $Result - $r
+		done
+        else
+                for ((r=$row1-1 ; r>-1 ; r-=1 ));
+		do
+		needsub=${NROW[$r]}
+		Result=$Result${needsub:0:1}
+		NROW[$r]=${needsub:1:${#needsub}}
+		#echo $Result - $r - d
+		done
+        fi
+	T=$[$T+1]
+done
 
+if [ $ucs -eq 1 ]; then
+	echo $Result > "decrypted_data.txt"
+fi
+echo $Result
 
 done < "$1"
 
